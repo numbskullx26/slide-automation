983a2d0a7c37d8bf4172f4bcb86ebe7f650a706d
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-12-20 13:56:04 +0530

    add : automations left grid layout UI

---
+++ src/components/global/automation-list/AutomationList.tsx
@@ -0,0 +1,18 @@
+// "use client";
+// import { Link } from "lucide-react";
+// import React from "react";
+
+// type Props = {};
+
+// const AutomationList = (props: Props) => {
+//   //WIP : GET THE AUTOMATON LIST DATA
+//   const { pathname } = usePaths();
+//   //WIP : if no automation exist show no automation
+//   return (
+//     <div className="flex flex-col gap-y-3">
+//       <Link href={}>
+      
+//       </Link>
+//     </div>
+//   )
+// };
