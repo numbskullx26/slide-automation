03068de970c98c4ee3389ebb4993be60f3dc5bf2
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2025-01-19 00:34:58 +0530

    add :  automations list UI changes based on th conditions and prefetched data

--- src/components/global/automation-list/AutomationList.tsx
+++ src/components/global/automation-list/AutomationList.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { usePaths } from "@/hooks/use-nav";
-import { cn } from "@/lib/utils";
+import { cn, getMonth } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 import GradientButton from "../gradient-button";
@@ -42,43 +42,53 @@ const AutomationList = (props: Props) => {
               This is from the Comment
             </p>
             {/* // WIP : AUTOMATON KEYWORDS */}
-            <div className="flex flex-wrap mt-3 gap-x-2">
-              <div
-                className={cn(
-                  "rounded-full px-4 py-1 capitalize",
-                  (0 + 1) % 2 == 0 &&
-                    "bg-keyword-green/15 border-2 bg-keyword-green",
-                  (1 + 1) % 2 == 0 &&
-                    "bg-keyword-purple/15 border-2 border-keyword-purple",
-                  (2 + 1) % 2 == 0 &&
-                    "bg-keyword-yellow/15 border-2 border-keyword-yellow",
-                  (3 + 1) % 2 == 0 &&
-                    "bg-keyword-red/15 border-2 border-keyword-red"
-                )}
-              >
-                Get Started
-              </div>  
-            </div>
-            <div className="rounded-full border-2 mt-3 border-dashed border-white/60 px-3 py-1">
-              <p className="text-sm text-[#bfc0c3]">No Keywords</p>
-            </div>
+            {automation.keywords.length > 0 ? (
+              <div className="flex flex-wrap mt-3 gap-x-2">
+                <div
+                  className={cn(
+                    "rounded-full px-4 py-1 capitalize",
+                    (0 + 1) % 2 == 0 &&
+                      "bg-keyword-green/15 border-2 bg-keyword-green",
+                    (1 + 1) % 2 == 0 &&
+                      "bg-keyword-purple/15 border-2 border-keyword-purple",
+                    (2 + 1) % 2 == 0 &&
+                      "bg-keyword-yellow/15 border-2 border-keyword-yellow",
+                    (3 + 1) % 2 == 0 &&
+                      "bg-keyword-red/15 border-2 border-keyword-red"
+                  )}
+                >
+                  Get Started
+                </div>
+              </div>
+            ) : (
+              <div className="rounded-full border-2 mt-3 border-dashed border-white/60 px-3 py-1">
+                <p className="text-sm text-[#bfc0c3]">No Keywords</p>
+              </div>
+            )}
           </div>
           <div className="flex flex-col justify-between">
             <p className="captalize text-s font-light text-[#9B9CA0]">
               {/* WIP : attach real date */}
-              October 10th 2024
+              {getMonth(automation.createdAt.getUTCMonth() + 1)}{" "}
+              {automation.createdAt.getUTCDate() === 1
+                ? `${automation.createdAt.getUTCDate()}st`
+                : `${automation.createdAt.getUTCDate()}th`}{" "}
+              {automation.createdAt.getUTCFullYear()}
             </p>
             {/* WIP : show the appropriate button based on the listener */}
-            <GradientButton
-              type="BUTTON"
-              className="w-full bg-background-80 hover:bg-background-80 text-white"
-            >
-              Smart AI
-            </GradientButton>
 
-            <Button className=" bg-background-80 hover:bg-background-80 text-white">
-              Standard
-            </Button>
+            {automation.listener?.listener === "SMARTAI" ? (
+              <GradientButton
+                type="BUTTON"
+                className="w-full bg-background-80 hover:bg-background-80 text-white"
+              >
+                Smart AI
+              </GradientButton>
+            ) : (
+              <Button className=" bg-background-80 hover:bg-background-80 text-white">
+                Standard
+              </Button>
+            )}
           </div>
         </Link>
       ))}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getMonth = (month: number) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  if (month < 1 || month > 12) {
+    return "Invalid month number. Please enter a number between 1 and 12";
+  }
+  return months[month - 1];
+};
